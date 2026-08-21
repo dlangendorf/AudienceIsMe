f50bc076f9c57e78bdea314a42433ea959137522
dlangendorf <dan.langendorf@gmail.com>
2026-08-21 14:24:57 -0500

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,9 +7,16 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <h1 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>
+        <img 
+          src={`${baseDir}/static/logo.png`} 
+          alt="Logo" 
+          style={{ height: "32px", width: "32px", marginRight: "8px", verticalAlign: "middle" }} 
+        />
+        {title}
+      </a>
+    </h1>
   )
 }
 
@@ -17,8 +24,8 @@ PageTitle.css = `
 .page-title {
   font-size: 1.75rem;
   margin: 0;
-  font-family: var(--titleFont);
 }
 `
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
+
