2a339b76ddb6d4e821ffe7142354750c6a963437
dlangendorf <dan.langendorf@gmail.com>
2026-08-25 15:45:39 -0500

    changes for component

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
     { Component: Component.TagList() },
-    Component.Explorer(),
+    { Component: Component.Explorer() },
   ],
   right: [
     Component.Graph(),
